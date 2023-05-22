c72e136c2ef60fe20b9da96383ce5b60dc5fb487
Lipon Roy <liponbdbd@gmail.com>
2023-05-22 20:08:55 +0600

    ,  operator use

--- Query-2.js
+++ Query-2.js
@@ -6,3 +6,26 @@ find({ price: { $gte: 5000, $lte: 9000 } }, { price: 1 });
 find({ price: { $in: [9000, 5000] } }, { price: 1 });
 // $nin  mane not in  mane array te ja ja ache oigulo chara dekhao
 find({ price: { $nin: [9000, 5000] } }, { price: 1 });
+
+// multiple condition sob milte hobe $and operator er moddhe
+// array er moddhe ekta ekta kore condition dite hobe
+// ekhane brand men, price 10000 er besi ebong 20000 er kom hote hobe
+
+find(
+	{
+		$and: [{ brand: 'men' }, { price: { $gt: 10000 } }, { price: { $lt: 20000 } }],
+	},
+	{ price: 1, brand: 1 },
+);
+
+// $or uporer $and er motoi but jekono ekta mach korlei dekhabe
+//  jemon eikhane brand men hole dekhabe  abar price 10000 er besi
+// holeo dekhabe price jodi women er moddheo 10000 er besi pay
+// setao dekhabe.
+
+const allProduct = await Product.find(
+	{
+		$or: [{ brand: 'men' }, { price: { $gt: 10000 } }],
+	},
+	{ price: 1, brand: 1 },
+);
